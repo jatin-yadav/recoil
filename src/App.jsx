93049b3c6b4b3b85ab9a4598bd2ba36f8a88b756
jatin-yadav <jatinyadav19oct@gmail.com>
2024-11-01 16:37:28 +0530

    Recoil

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,56 @@
-/* eslint-disable react/prop-types */
-import { useState } from "react";
+/* eslint-disable react/display-name */
 import "./App.css";
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
+import { counterAtom } from "../src/store/atoms/counterAtom";
+import { memo } from "react";
 
 function App() {
   return (
+    <RecoilRoot>
+      <Counter1 />
+      <Counter2 />
+    </RecoilRoot>
+  );
+}
+export default App;
+
+const Counter1 = () => {
+  return (
     <>
-      <Counter />
+      <Value />
+      <Incrase />
+      <Decrease />
     </>
+  );
+};
+
+const Counter2 = () => {
+  return (
+    <>
+      <Value />
+      <div>
+        <Incrase />
+        <Decrease />
+      </div>
+    </>
+  );
+};
+
+function Decrease() {
+  const setCount = useSetRecoilState(counterAtom);
+  return (
+    <button onClick={() => setCount((count) => count - 1)}>Decrease</button>
   );
 }
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
-
+const Incrase = memo(function () {
+  const setCount = useSetRecoilState(counterAtom);
   return (
-    <>
-      <div>
-        <CounterDisplay count={count} />
-      </div>
-      <div style={{ display: "flex" }}>
-        <Increment setCount={setCount} />
-        <Dcrement setCount={setCount} />
-      </div>
-    </>
+    <button onClick={() => setCount((count) => count + 1)}>Increase</button>
   );
-};
+});
 
-const CounterDisplay = ({ count }) => {
-  return (
-    <div>
-      <h1>{count}</h1>
-    </div>
-  );
-};
-
-const Increment = ({ setCount }) => {
-  return (
-    <div>
-      <button onClick={() => setCount((i) => i + 1)}>Increase</button>
-    </div>
-  );
-};
-
-const Dcrement = ({ setCount }) => {
-  return (
-    <div>
-      <button onClick={() => setCount((i) => i - 1)}>Dcrease</button>
-    </div>
-  );
-};
-export default App;
+function Value() {
+  const countValue = useRecoilValue(counterAtom);
+  return <p>Count: {countValue}</p>;
+}
